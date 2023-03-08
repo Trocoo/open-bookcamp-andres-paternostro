624aaac199daa0a2ee14f76e33f360a42027bf1a
Trocoo <126356496+Trocoo@users.noreply.github.com>
2023-03-08 15:29:19 -0500

    Add files via upload

---
+++ Ejercicio 2 Open Bookcamp - Curso de JavaScript Basico.js
@@ -0,0 +1,18 @@
+//Ejercicio 2 - Open Bookcamp
+
+const nombre = "Andres";
+const apellido = "Patenostro";
+const edad = 24
+const desarrollador = false
+
+const fecha_cadena = new Date("may 31 1998");
+fecha_de_nacimiento = fecha_cadena;
+
+const libro_favorito = {
+titulo: "Steve Jobs",
+autor: " Walter Isaacson",
+fecha: "21 de noviembre de 2011",
+url: "https://www.panamericana.com.co/steve-jobs/p",
+}
+
+console.log(libro_favorito)
