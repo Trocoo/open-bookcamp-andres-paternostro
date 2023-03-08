e5b7595e970ebda0d1f505ac76208b159435c2c7
Trocoo <126356496+Trocoo@users.noreply.github.com>
2023-03-08 12:47:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // este es el mensaje de bienvenida
 console.log("esta es la puerta de entrada al proyecto")
 
-
-
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo.")
 
 // este es el mensaje de despedida
 console.log("adios")
-
